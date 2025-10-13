8eee7a09654bbeb6a81f6596bc9b404a232582ef
karmaxx2024 <karmaxx2023@gmail.com>
2025-10-13 16:26:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('welcome.html')
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5001, ssl_context=('cert.pem', 'key.pem'))
